f73411cea794d48f6c164706656ba0085b4a198b
Furkan Yıldız <realfurkanyldz@gmail.com>
2022-07-25 00:45:35 +0300

    adding elements and designing table

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/no-inline-styles */
 import {
   Text,
   View,
@@ -5,17 +6,23 @@ import {
   Image,
   StyleSheet,
   TouchableOpacity,
+  ScrollView,
+  SafeAreaView
 } from 'react-native';
 import React, {useState} from 'react';
 
-const picsumImages = new Array(126).fill('http://placeimg.com/640/360/any');
+const picsumImages = new Array(110).fill('H');
+const customData = require('./assets/periodictable.json');
 
 function renderItem({item}) {
   return (
-    <TouchableOpacity>
-      <View style={styles.container}>
+    <TouchableOpacity style={item.key == 'empty' ? styles.empty : styles.test}>
+      <View style={item.key == 'empty' ? {opacity: 0} : styles.container}>
         <View style={styles.border}>
-          <Image source={{uri: item}} style={styles.image} />
+          <View style={styles.item}>
+            <Text style={{fontSize: 8}}>{item.number}</Text>
+            <Text style={{fontSize: 16}}>{item.symbol}</Text>
+          </View>
         </View>
       </View>
     </TouchableOpacity>
@@ -24,15 +31,18 @@ function renderItem({item}) {
 const Periodium = () => {
   const [images, setImages] = React.useState(picsumImages);
   return (
-    <View style={styles.list}>
-      <Text style={styles.text}>Periodium</Text>
-      <FlatList
-        data={images}
-        renderItem={renderItem}
-        numRows={7}
-        numColumns={18}
-      />
-    </View>
+    <SafeAreaView>
+      <View style={styles.list}>
+        <Text style={styles.text}>Periodium</Text>
+        <FlatList
+          data={customData.elements}
+          renderItem={renderItem}
+          numRows={7}
+          numColumns={18}
+          style={styles.flatList}
+        />
+      </View>
+    </SafeAreaView>
   );
 };
 
@@ -40,22 +50,37 @@ const styles = StyleSheet.create({
   list: {
     alignItems: 'center',
   },
+  flatList: {},
+  flatList2: {
+    position: 'absolute',
+    top: 260,
+    left: 160,
+  },
   container: {
     padding: 2,
   },
+  empty: {
+    padding: 2,
+  },
   border: {
-    backgroundColor: '#87ceeb',
     padding: 1,
+    opacity: 100,
   },
   text: {
     alignText: 'center',
-    padding: 5,
+    paddingTop: 10,
     fontSize: 20,
+    color: 'white',
   },
-  image: {
+
+  item: {
     height: 36,
     width: 36,
-    opacity: 0,
+    borderRadius: 8,
+    paddingLeft: 4,
+    justifyContent: 'center',
+    backgroundColor: 'mediumspringgreen',
   },
+  test: {},
 });
 export default Periodium;
